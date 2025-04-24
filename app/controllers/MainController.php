6596ab95c563893e7249d907357923d64bc85c4e
Mariyam Tunkara <Mariyamtunkara456@gmail.com>
2025-04-24 00:57:23 -0400

    Update MainController.php

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -7,12 +7,12 @@ class MainController extends Controller {
     public function homepage() {
         // Remember to route relative to index.php
         // Require page and exit to return an HTML page
-        $this->returnView('./assets/views/main/homepage.html'); // Adjust path as necessary
+        $this->returnView('./assets/views/main/homepage.html'); 
     }
 
     public function notFound() {
         http_response_code(404);
         $this->returnView('./assets/views/main/notFound.html');
-        echo "404 Not Found"; // You can also return a view for a custom 404 page
+        echo "404 Not Found"; 
     }
 }
