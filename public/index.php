16a4010311fd84074a74001a83fae88cc27ca762
Mariyam Tunkara <Mariyamtunkara456@gmail.com>
2025-04-24 00:58:49 -0400

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,16 +1,15 @@
 <?php
 
-require '../app/core/setup.php'; // Ensure this file sets up your environment correctly
-
+require '../app/core/setup.php'; 
 use app\core\Router;
 use app\controllers\MainController;
 use app\controllers\BookingController;
 
-// Load routes from routes.php
-$routes = require '../app/core/routes.php'; // Ensure this path is correct
 
-// Create a new Router instance with the routes
-$router = new Router($routes); // Pass the routes to the constructor
+$routes = require '../app/core/routes.php';
 
-// Serve the route based on the request
+
+$router = new Router($routes); 
+
+
 $router->serveRoute();
